1756c1fdb28d3db18b8fc1d1cd4374d42fbb8fc1
enstrteam <enstrteam@gmail.com>
2025-01-12 22:58:58 +0300

    renamed:    src/router/index.js -> src/router/index.ts 	new file:   src/shims-vue.d.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router';
 import Home from '../views/Home.vue';
 import Boosters from '../views/Boosters.vue';
 import Referrals from '../views/Referrals.vue';
 import Tasks from '../views/Tasks.vue';
 import Awards from '../views/Awards.vue';
 
-const routes = [
+const routes: Array<RouteRecordRaw> = [
   { path: '/', name: 'Home', component: Home },
   { path: '/boosters', name: 'Boosters', component: Boosters },
   { path: '/referrals', name: 'Referrals', component: Referrals },
